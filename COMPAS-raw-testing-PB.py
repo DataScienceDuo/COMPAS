2e7b9d512d4eab3fcc68482f08879a3537710f4c
Pablo Boix <pboix@me.com>
2020-06-05 23:08:50 -0400

    Test changes by Pablo

---
+++ COMPAS-raw-testing-PB.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+
+COMPAS_RAW_IN0 = pd.read_csv(r'/Users/pboix/Documents/GitHub/COMPAS/compas-scores-raw.csv')
+
+COMPAS_RAW_IN0
+
