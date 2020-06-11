e308301089bddea5cb579d00189d6b4d7565efb9
Pablo Boix <pboix@me.com>
2020-06-11 04:18:55 -0400

    Added some code both in R and .py file to execute the data wrangling removing the 4 PersonID with "bad" birth dates

--- COMPAS-raw-testing-PB.py
+++ COMPAS-raw-testing-PB.py
@@ -7,8 +7,26 @@ This is a temporary script file.
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from pylab import *
+import seaborn as sns
+# %matplotlib inline
+import  statsmodels.api as sm
+import statsmodels.stats.api as sms
+from scipy.stats import boxcox
 
-COMPAS_RAW_IN0 = pd.read_csv(r'/Users/pboix/Documents/GitHub/COMPAS/compas-scores-raw.csv')
+compas_raw_in0 = pd.read_csv(r'C:\Users\pablo\OneDrive\Escritorio\Proyecto Final WozU Git\COMPAS\compas-scores-raw.csv')
 
-COMPAS_RAW_IN0
+compas_raw_in0
 
+print(compas_raw_in0)
+
+# Here was testing options to remove "bad" ID´s
+# compas_raw_in1 = compas_raw_in0.query("Person_ID != '51157'")
+# compas_raw_in2 = compas_raw_in0[compas_raw_in0['Person_ID'] != 51157]
+# compas_raw_in3 = compas_raw_in0[compas_raw_in0['Person_ID'].ne(51157)]
+# sales.drop(sales[sales.CustomerId.isin(badcu)].index.tolist())
+
+# We remove the inmates with the Person_ID we discard
+badID= [51157, 57823, 62384, 54272]
+compas_raw_in4 = compas_raw_in0[~compas_raw_in0.Person_ID.isin(badID)]
---
+++ R-testing-PB.R
@@ -0,0 +1,43 @@
+library(readr)
+library(IDPmisc)
+
+
+compas_scores_raw <- read_csv("C:/Users/pablo/OneDrive/Escritorio/Proyecto Final WozU Git/COMPAS/compas-scores-raw.csv")
+View(compas_scores_raw)
+
+
+compas_scores2 <- na.omit(compas_scores_raw)
+
+# To remove NA´s fields
+compas_scores3 <- NaRV.omit(compas_scores_raw)
+# No change.
+
+# We remove rows for Persons with "weird" birthdates 
+compas_scores4 <-compas_scores3[!(compas_scores3$Person_ID=="51157" | compas_scores3$Person_ID=="57823"),]
+compas_scores5 <-compas_scores4[!(compas_scores4$Person_ID=="62384" | compas_scores4$Person_ID=="54272"),]
+
+# examples of removing rows using a list/vector
+# install.packages("Hmisc")
+# library("Hmisc")
+# datawithoutVF = data[which(rownames(data) %nin% remove), ]
+# datawithoutVF = data[!row.names(data)%in%remove,]
+# datawithoutVF = data[ !(row.names(data) %in% remove), ]
+
+# Libraries needed for regression. (not in use YET, for next steps use)
+install.packages("caret")
+install.packages("e1071")
+install.packages("predictmeans")
+install.packages("gvlma")
+install.packages("popbio")
+
+library("car")
+library("caret")
+library("gvlma")
+library("predictmeans")
+library("e1071")
+library("magrittr")
+library("dplyr")
+library("tidyr")
+library("lmtest")
+library("popbio")
+
