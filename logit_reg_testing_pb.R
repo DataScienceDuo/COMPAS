3767033e0dc0ac6711c94e6b908d22011783dde0
Pablo Boix <pboix@me.com>
2020-06-15 13:39:51 -0400

    Update

--- logit_reg_testing_pb.R
+++ logit_reg_testing_pb.R
@@ -13,6 +13,7 @@ logit_input <- datasets_1498_2680_propublicaCompassRecividism_data_fairml_csv_pr
 
 # We will check how good predictors are some IV (like race, etc)
 # Now we do a trial with a one-to-one (one IV, one DV)
+
 ylogit_propub <- glm(Two_yr_Recidivism ~ ., data=logit_input, family="binomial")
 
 summary(ylogit_propub)
@@ -41,5 +42,53 @@ logisticPseudoR2s <- function(LogModel) {
 
 
 logisticPseudoR2s(ylogit_propub)
+
+step(ylogit_propub,direction = 'backward')
+
+# Backward step doesn´t "keep" african. (HOWEVER KEEPS SEX AND AGE ?? hOW DO WE DRAW ADITIONAL CONCLUTIONS ON THIS?)
+# We need to discuss if the remaing factors lead us to any conclusion
+# ################
+# ################ RESULT AFTER LAST STEP:
+# ################
+# Step:  AIC=7470.05
+# Two_yr_Recidivism ~ Number_of_Priors + score_factor + Age_Above_FourtyFive + 
+#   Age_Below_TwentyFive + Female + Misdemeanor
+# 
+# Df Deviance    AIC
+# <none>                      7456.1 7470.1
+# - Misdemeanor           1   7465.4 7477.4
+# - Female                1   7482.9 7494.9
+# - Age_Above_FourtyFive  1   7502.9 7514.9
+# - Age_Below_TwentyFive  1   7513.5 7525.5
+# - score_factor          1   7588.1 7600.1
+# - Number_of_Priors      1   7765.7 7777.7
+# 
+# Call:  glm(formula = Two_yr_Recidivism ~ Number_of_Priors + score_factor + 
+#              Age_Above_FourtyFive + Age_Below_TwentyFive + Female + Misdemeanor, 
+#            family = "binomial", data = logit_input)
+# 
+# Coefficients:
+#   (Intercept)      Number_of_Priors          score_factor  Age_Above_FourtyFive  Age_Below_TwentyFive                Female           Misdemeanor  
+# -0.8046                0.1327                0.7187               -0.5197                0.5420               -0.3741               -0.1814  
+# 
+# Degrees of Freedom: 6171 Total (i.e. Null);  6165 Residual
+# Null Deviance:	    8506 
+# Residual Deviance: 7456 	AIC: 7470
+
+
+# we REMOVE SOME COLUMNS : 
+# wE NEED TO RUN "R-testing-PB.R" to load the needed dataset
+#
+#
+head(compas_scores5)
+# Next line we create a vector with all colunms to be removed.
+dropfromraw <- c("Person_ID", "AssessmentID", "Case_ID", "LastName", "FirstName", "MiddleName", "Screening_Date", "ScaleSet", "Screening_Date", "RecSupervisionLevelText", "DisplayText", "RawScore", "AssessmentReason","IsCompleted", "IsDeleted" )
+compas_scores_redcol = compas_scores5[,!(names(compas_scores5) %in% dropfromraw)]
+head(compas_scores_redcol)
+ylogit_compas_scores_redcol <- glm(DecileScore ~ ., data=compas_scores_redcol)
+
+
+
+
 
 
